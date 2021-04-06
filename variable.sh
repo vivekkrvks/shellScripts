b21c18826455e06d3eb8e16aab50d99467662b6c
vivekkrvks <vivek.kr.vks@gmail.com>
2021-04-06 12:57:31 +0530

    second commit

---
+++ variable.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# default variable in shell script
+
+echo "Below are some default variable result"
+
+echo $BASH #VARIABLE TO SEE CURRENT BASH PATH
+echo $BASH_VERSION # to see bash version
+echo $HOME # to see current user home directory
+echo $PWD # to see present working directory
+
+# varible define by user
+
+# number infront of varible is not allowed like you can't consider 10val as variable
+
+name = Vivek
+
+echo My name is $name
